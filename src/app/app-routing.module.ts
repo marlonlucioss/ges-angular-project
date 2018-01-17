1db230b1125e0fb45aa9d9f47f9d75d9923f7ebc
Leandro Vitor <vitoleandro@gmail.com>
2018-01-17 17:04:44 -0300

    GES-3 #comment login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './exceptions/not-found/not-found.component';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'app',
-    pathMatch: 'full'
-  },
-  {
-    path: 'app',
-    component: AppComponent
-  },
-  {
-    path: '**',
-    component: NotFoundComponent
-  }
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: LoginPageComponent },
+  { path: 'app', component: AppComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-=======
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule  } from '@angular/material';
->>>>>>> feature/ui-settings
-
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './exceptions/not-found/not-found.component';
-
 import { AuthService } from './auth/auth.service';
-
 import { AppHttpInterceptor } from './app-http-interceptor';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoginPageComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [
     AuthService,
@@ -35,15 +38,6 @@ import { AppHttpInterceptor } from './app-http-interceptor';
       useClass: AppHttpInterceptor,
       multi: true
     }
-=======
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule
->>>>>>> feature/ui-settings
   ],
   bootstrap: [AppComponent]
 })

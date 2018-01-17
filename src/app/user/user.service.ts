199d8a7b05ed27cbe9d144a3550bb50e645c5a0c
Marlon Lucio <marlonlucioss@gmail.com>
2018-01-17 16:17:55 -0300

    GES-5 #progress #time 25m #comment Adding the CRUD methods on user service

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -9,14 +9,15 @@ export class UserService {
   constructor(public http: HttpRequest, public auth: AuthService) { }
 
   /**
-   * Method to do logout
+   * Method to do login
+   * @param userData
    */
   public login(userData) {
     // Send the request to logout
-    this.http.post('/login', data).subscribe(
-      success => {
+    this.http.post('/user/login', userData).subscribe(
+      success  => {
         // Remove the user from the local session
-        this.auth.addUserSession(data);
+        this.auth.addUserSession(success);
         console.log('Successfully Completed');
         console.log(success);
       },
@@ -32,12 +33,80 @@ export class UserService {
    */
   public logout() {
     // Send the request to logout
-    this.http.get('/logout').subscribe(
+    this.http.delete('/user/logout').subscribe(
       success => {
         // Remove the user from the local session
         this.auth.removeUser();
         console.log('Successfully Completed');
         console.log(success);
+      },
+      error => {
+        console.log('Not Completed');
+        console.log(error);
+      }
+    );
+  }
+
+  /**
+   * Method to do logout
+   */
+  public add(userData) {
+    // Send the request to add a new user
+    this.http.post('/user/add', userData).subscribe(
+      success => {
+        // Return the new user object
+        return success;
+      },
+      error => {
+        console.log('Not Completed');
+        console.log(error);
+      }
+    );
+  }
+
+  /**
+   * Method to do logout
+   */
+  public remove(id) {
+    // Send the request to remove an user
+    this.http.put('/user/remove', id).subscribe(
+      success => {
+        // Remove request object
+        return success;
+      },
+      error => {
+        console.log('Not Completed');
+        console.log(error);
+      }
+    );
+  }
+
+  /**
+   * Method to do logout
+   */
+  public fetch() {
+    // Send the request to get the user list
+    this.http.get('/user/list').subscribe(
+      success => {
+        // Return the success object
+        return success;
+      },
+      error => {
+        console.log('Not Completed');
+        console.log(error);
+      }
+    );
+  }
+
+  /**
+   * Method to do logout
+   */
+  public edit(userData) {
+    // Send the request to edit user
+    this.http.put('/user/edit', userData).subscribe(
+      success => {
+        // Return the success object
+        return success;
       },
       error => {
         console.log('Not Completed');

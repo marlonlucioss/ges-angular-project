8339a795cd9f775de2cc21bded5ad14261205879
Leandro Vitor <vitoleandro@gmail.com>
2018-02-06 10:46:38 -0300

    GES-26 #done #time 32m #comment Chekc if user is logged on app and hidde components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,26 @@ import { AuthService } from '@auth/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(public auth: AuthService, public router: Router, private translate: TranslateService) {
-    translate.setDefaultLang('pt-br');
+
+  hasLogged;
+
+  constructor(
+    private auth: AuthService, 
+    private router: Router, 
+    private translate: TranslateService) {
+    translate.setDefaultLang('pt-br'); 
   }
   ngOnInit() {
     if (!this.auth.isLogged()) {
       this.router.navigateByUrl('/login');
+    }
+  }
+
+  ngDoCheck() {
+    if(this.auth.isLogged()) {
+      this.hasLogged = true;
+    } else {
+      this.hasLogged = false;
     }
   }
 }
